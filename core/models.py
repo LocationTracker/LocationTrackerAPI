9b79942ca1b69fa48ba9553a913d14c65817dc09
RodrigoNdeC <rodrigondec@gmail.com>
2018-05-13 17:57:29 -0300

    Familia criada

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+from django.db.models import Model, CharField, ImageField, OneToOneField, ForeignKey, PROTECT, SET_NULL
+from django.contrib.auth.models import User
+from .utils import get_usuario_upload_path
 
-# Create your models here.
+
+class Familia(Model):
+    nome = CharField(max_length=30, null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
+
+
