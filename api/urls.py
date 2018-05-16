660b4adc86832549eaca7180c272c45cc6af8848
RodrigoNdeC <rodrigondec@gmail.com>
2018-05-15 21:48:30 -0300

    api urls ok

---
+++ api/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from rest_framework import routers
+from rest_framework_swagger.views import get_swagger_view
+from core.views import FamiliaViewSet, UsuarioViewSet, ListUsers
+
+router = routers.DefaultRouter()
+router.register('familias', FamiliaViewSet)
+router.register('usuarios', UsuarioViewSet)
+# router.register('user', ListUsers.as_view())
+
+schema_view = get_swagger_view(title='LocationTracker API')
+
+urlpatterns = [
+    path('', schema_view),
+    path('router/', include(router.urls)),
+    path('auth/', include('rest_framework.urls'))
+]
