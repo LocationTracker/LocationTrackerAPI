8d5654e227d5e66e4b168b74f984fa1dab51f013
RodrigoNdeC <rodrigondec@gmail.com>
2018-05-22 14:39:20 -0300

    LocationViewSet add

--- location/views.py
+++ location/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import LocalizacaoSerializer
+from .models import Localizacao
 
-# Create your views here.
+
+class LocalizacaoViewSet(viewsets.ModelViewSet):
+    """
+    retrieve:
+        Return a user instance.
+    list:
+        Return all users, ordered by most recently joined.
+    create:
+        Create a new user.
+    delete:
+        Remove an existing user.
+    partial_update:
+        Update one or more fields on an existing user.
+    update:
+        Update a user.
+    """
+    queryset = Localizacao.objects.all()
+    serializer_class = LocalizacaoSerializer
+    # permission_classes = [
+    #     IsAuthenticated,
+    # ]
+    # http_method_names = ['get', 'post', 'put', 'delete']
