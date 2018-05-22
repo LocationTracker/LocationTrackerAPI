092d3ea21efb590c86931271f14478ce61ff1fd7
RodrigoNdeC <rodrigondec@gmail.com>
2018-05-22 14:40:17 -0300

    LocalizacaoViewSet adicionado

--- api/urls.py
+++ api/urls.py
@@ -2,10 +2,12 @@ from django.urls import path, include
 from rest_framework import routers
 from rest_framework_swagger.views import get_swagger_view
 from core.views import FamiliaViewSet, UsuarioViewSet, ListUsers
+from location.views import LocalizacaoViewSet
 
 router = routers.DefaultRouter()
 router.register('familias', FamiliaViewSet)
 router.register('usuarios', UsuarioViewSet)
+router.register('localizacoes', LocalizacaoViewSet)
 # router.register('user', ListUsers.as_view())
 
 schema_view = get_swagger_view(title='LocationTracker API')
