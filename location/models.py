6cc5a3c4bd1a3971312fe9caf6d5589bd76e6add
RodrigoNdeC <rodrigondec@gmail.com>
2018-05-28 20:38:29 -0300

    get filtros

--- location/models.py
+++ location/models.py
@@ -1,5 +1,6 @@
 from mongoengine import Document, EmbeddedDocument, StringField, IntField, EmbeddedDocumentField, EmbeddedDocumentListField, MapField
 
+
 class Localizacao(EmbeddedDocument):
     minutos = StringField()
     lat = StringField()
@@ -12,6 +13,9 @@ class Localizacao(EmbeddedDocument):
 class HoraLocalizacao(EmbeddedDocument):
     value = StringField()
     localizacoes = MapField(EmbeddedDocumentField(Localizacao))
+
+    def __str__(self):
+        return 'Hora {}'.format(self.value)
 
 
 class DiaLocalizacao(EmbeddedDocument):
@@ -45,7 +49,7 @@ class UsuarioLocalizacao(Document):
     def __str__(self):
         return "Localizações de {}".format(self.id_usuario)
 
-    def _vefify_ano(self, ano):
+    def _verify_ano(self, ano):
         if ano in self.anos:
             return True
         return False
@@ -62,6 +66,11 @@ class UsuarioLocalizacao(Document):
 
     def _verify_hora(self, ano, mes, dia, hora):
         if hora in self.anos[ano].meses[mes].dias[dia].horas:
+            return True
+        return False
+
+    def _verify_minutos(self, ano, mes, dia, hora, minutos):
+        if minutos in self.anos[ano].meses[mes].dias[dia].horas[hora].localizacoes[minutos]:
             return True
         return False
 
@@ -88,8 +97,33 @@ class UsuarioLocalizacao(Document):
         a.meses[m.value] = m
         return a
 
+    def get_ano(self, ano):
+        if self._verify_ano(ano):
+            return self.anos[ano]
+        return None
+
+    def get_mes(self, ano, mes):
+        if self._verify_mes(ano, mes):
+            return self.anos[ano].meses[mes]
+        return None
+
+    def get_dia(self, ano, mes, dia):
+        if self._verify_dia(ano, mes, dia):
+            return self.anos[ano].meses[mes].dias[dia]
+        return None
+
+    def get_hora(self, ano, mes, dia, hora):
+        if self._verify_hora(ano, mes, dia, hora):
+            return self.anos[ano].meses[mes].dias[dia].horas[hora]
+        return None
+
+    def get_minutos(self, ano, mes, dia, hora, minutos):
+        if self._verify_minutos(ano, mes, dia, hora, minutos):
+            return self.anos[ano].meses[mes].dias[dia].horas[hora].localizacoes[minutos]
+        return None
+
     def add_location(self, ano, mes, dia, hora, loc):
-        if self._vefify_ano(ano):
+        if self._verify_ano(ano):
             print('has ano')
             if self._verify_mes(ano, mes):
                 print('has mes')
