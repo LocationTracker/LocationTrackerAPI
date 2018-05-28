6280796b292f50462bff71366059540506167b80
RodrigoNdeC <rodrigondec@gmail.com>
2018-05-28 19:45:27 -0300

    modelo funcionando

--- location/models.py
+++ location/models.py
@@ -1,157 +1,112 @@
-from djongo.models import Model as Document, CharField as StringField, IntegerField as IntField, ArrayModelField
+from mongoengine import Document, EmbeddedDocument, StringField, IntField, EmbeddedDocumentField, EmbeddedDocumentListField, MapField
 
-
-class Localizacao(Document):
-    hora = IntField()
-    minutos = IntField()
-    lat = StringField(max_length=30)
-    long = StringField(max_length=30)
+class Localizacao(EmbeddedDocument):
+    minutos = StringField()
+    lat = StringField()
+    long = StringField()
 
     def __str__(self):
-        return "{}:{} - {} e {}".format(self.hora, self.minutos, self.lat, self.long)
-
-    class Meta:
-        unique_together = ('hora', 'minutos')
-        abstract = True
+        return "{} - {} e {}".format(self.minutos, self.lat, self.long)
 
 
-class DiaLocalizacao(Document):
-    value = IntField()
-    localizacoes = ArrayModelField(
-        model_container=Localizacao,
-        default=[]
-    )
+class HoraLocalizacao(EmbeddedDocument):
+    value = StringField()
+    localizacoes = MapField(EmbeddedDocumentField(Localizacao))
+
+
+class DiaLocalizacao(EmbeddedDocument):
+    value = StringField()
+    horas = MapField(EmbeddedDocumentField(HoraLocalizacao))
 
     def __str__(self):
         return 'Dia {}'.format(self.value)
 
-    class Meta:
-        abstract = True
 
-
-class MesLocalizacao(Document):
-    value = IntField()
-    dias = ArrayModelField(
-        model_container=DiaLocalizacao,
-        default=[]
-    )
+class MesLocalizacao(EmbeddedDocument):
+    value = StringField()
+    dias = MapField(EmbeddedDocumentField(DiaLocalizacao))
 
     def __str__(self):
         return 'Mes {}'.format(self.value)
 
-    class Meta:
-        abstract = True
 
-
-class AnoLocalizacao(Document):
-    value = IntField()
-    meses = ArrayModelField(
-        model_container=MesLocalizacao,
-        default=[]
-    )
+class AnoLocalizacao(EmbeddedDocument):
+    value = StringField()
+    meses = MapField(EmbeddedDocumentField(MesLocalizacao))
 
     def __str__(self):
         return 'Ano {}'.format(self.value)
 
-    class Meta:
-        abstract = True
-
 
 class UsuarioLocalizacao(Document):
     id_usuario = IntField(unique=True)
-    anos = ArrayModelField(
-        model_container=AnoLocalizacao,
-        default=[]
-    )
+    anos = MapField(EmbeddedDocumentField(AnoLocalizacao))
 
     def __str__(self):
         return "Localizações de {}".format(self.id_usuario)
 
-    def get_ano_status(self, ano):
-        for index in range(0, len(self.anos)):
-            if self.anos[index].value == ano:
-                return {'has': True, 'index': index}
-        return {'has': False}
+    def _vefify_ano(self, ano):
+        if ano in self.anos:
+            return True
+        return False
 
-    def get_mes_status(self, ano_index, mes):
-        for index in range(0, len(self.anos[ano_index].meses)):
-            if self.anos[ano_index].meses[index].value == mes:
-                return {'has': True, 'index': index}
-        return {'has': False}
+    def _verify_mes(self, ano, mes):
+        if mes in self.anos[ano].meses:
+            return True
+        return False
 
-    def get_dia_status(self, ano_index, mes_index, dia):
-        for index in range(0, len(self.anos[ano_index].meses[mes_index].dias)):
-            if self.anos[ano_index].meses[mes_index].dias[index].value == dia:
-                return {'has': True, 'index': index}
-        return {'has': False}
+    def _verify_dia(self, ano, mes, dia):
+        if dia in self.anos[ano].meses[mes].dias:
+            return True
+        return False
 
-    def get_time_status(self, ano_index, mes_index, dia_index, hora, minutos):
-        for index in range(0, len(self.anos[ano_index].meses[mes_index].dias[dia_index].localizacoes)):
-            if self.anos[ano_index].meses[mes_index].dias[dia_index].localizacoes[index].hora == hora \
-                    and self.anos[ano_index].meses[mes_index].dias[dia_index].localizacoes[index].minutos == minutos:
-                return {'has': True, 'index': index}
-        return {'has': False}
+    def _verify_hora(self, ano, mes, dia, hora):
+        if hora in self.anos[ano].meses[mes].dias[dia].horas:
+            return True
+        return False
 
-    def get_data_status(self, ano, mes, dia, hora, minutos):
-        msg = {'ano': self.get_ano_status(ano)}
-        if msg['ano']['has']:
-            msg['mes'] = self.get_mes_status(msg['ano']['index'], mes)
-            if msg['mes']['has']:
-                msg['dia'] = self.get_dia_status(msg['ano']['index'], msg['mes']['index'], dia)
-                if msg['dia']['has']:
-                    msg['time'] = self.get_time_status(msg['ano']['index'], msg['mes']['index'],
-                                                       msg['dia']['index'], hora, minutos)
-        return msg
+    def _create_hora(self, hora, loc):
+        h = HoraLocalizacao(value=hora)
+        h.localizacoes[loc.minutos] = loc
+        return h
 
-    def add_localizacao(self, ano, mes, dia, localizacao):
-        msg = {
-            'ano': {'value': ano},
-            'mes': {'value': mes},
-            'dia': {'value': dia},
-            'localizacao': {'value': localizacao}
-        }
-        data_status = self.get_data_status(ano, mes, dia, localizacao.hora, localizacao.minutos)
-        msg['data_status'] = data_status
+    def _create_dia(self, dia, hora, loc):
+        d = DiaLocalizacao(value=dia)
+        h = self._create_hora(hora, loc)
+        d.horas[h.value] = h
+        return d
 
-        if data_status['ano']['has']:
-            ANO_INDEX = data_status['ano']['index']
+    def _create_mes(self, mes, dia, hora, loc):
+        m = MesLocalizacao(value=mes)
+        d = self._create_dia(dia, hora, loc)
+        m.dias[d.value] = d
+        return m
 
-            if data_status['mes']['has']:
-                MES_INDEX = data_status['mes']['index']
+    def _create_ano(self, ano, mes, dia, hora, loc):
+        a = AnoLocalizacao(value=ano)
+        m = self._create_mes(mes, dia, hora, loc)
+        a.meses[m.value] = m
+        return a
 
-                if data_status['dia']['has']:
-                    DIA_INDEX = data_status['dia']['index']
-
-                    if data_status['time']['has']:
-                        TIME_INDEX = data_status['time']['index']
-
-                        msg['localizacao']['old'] = self.anos[ANO_INDEX].meses[MES_INDEX]. \
-                            dias[DIA_INDEX].localizacoes[TIME_INDEX]
-
-                        self.anos[ANO_INDEX].meses[MES_INDEX].dias[DIA_INDEX].localizacoes[TIME_INDEX] = localizacao
+    def add_location(self, ano, mes, dia, hora, loc):
+        if self._vefify_ano(ano):
+            print('has ano')
+            if self._verify_mes(ano, mes):
+                print('has mes')
+                if self._verify_dia(ano, mes, dia):
+                    print('has dia')
+                    if self._verify_hora(ano, mes, dia, hora):
+                        print('has hora')
+                        self.anos[ano].meses[mes].dias[dia].horas[hora].localizacoes[loc.minutos] = loc
                     else:
-                        self.anos[ANO_INDEX].meses[MES_INDEX].dias[DIA_INDEX].localizacoes.append(localizacao)
+                        print('create hora')
+                        self.anos[ano].meses[mes].dias[dia].horas[hora] = self._create_hora(hora, loc)
                 else:
-                    self.anos[ANO_INDEX].meses[MES_INDEX].dias.append(
-                        DiaLocalizacao(value=dia, localizacoes=[localizacao])
-                    )
+                    print('create dia')
+                    self.anos[ano].meses[mes].dias[dia] = self._create_dia(dia, hora, loc)
             else:
-                self.anos[ANO_INDEX].meses.append(
-                    MesLocalizacao(value=mes, dias=[
-                        DiaLocalizacao(value=dia, localizacoes=[localizacao])
-                    ])
-                )
+                print('create mes')
+                self.anos[ano].meses[mes] = self._create_mes(mes, dia, hora, loc)
         else:
-            self.anos.append(
-                AnoLocalizacao(value=ano, meses=[
-                    MesLocalizacao(value=mes, dias=[
-                        DiaLocalizacao(value=dia, localizacoes=[localizacao])
-                    ])
-                ])
-            )
-
-        return msg
-
-    class Meta:
-        verbose_name = 'localizações usuário'
-        verbose_name_plural = 'localizações usuários'
+            print('create ano')
+            self.anos[ano] = self._create_ano(ano, mes, dia, hora, loc)
