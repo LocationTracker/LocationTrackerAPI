33dbc7b6eff01285491fa6f5e4aafa3e3e80771a
RodrigoNdeC <rodrigondec@gmail.com>
2018-06-22 01:19:23 -0300

    include token view

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework.authtoken import views as TokenView
 from rest_framework_swagger.views import get_swagger_view
 from core.views import UsuarioViewSet
 
@@ -11,5 +12,6 @@ schema_view = get_swagger_view(title='LocationTracker API')
 urlpatterns = [
     path('', schema_view),
     path('router/', include(router.urls)),
-    path('auth/', include('rest_framework.urls'))
+    path('auth/', include('rest_framework.urls')),
+    path('router/auth/', TokenView.obtain_auth_token)
 ]
