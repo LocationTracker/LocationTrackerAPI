cead256cd07b1465dfe113cca6aa6fbe6fecdc55
RodrigoNdeC <rodrigondec@gmail.com>
2018-06-27 20:01:17 -0300

    removido app

--- LocationTrackerAPP/app/src/main/java/br/ufrn/locationtracker/Api/OnJSONArrayResponseCallback.java
+++
@@ -1,6 +0,0 @@
-package br.ufrn.locationtracker.Api;
-import org.json.JSONArray;
-
-public interface OnJSONArrayResponseCallback {
-    void onJSONArrayResponse(boolean success, JSONArray response);
-}
--- LocationTrackerAPP/app/src/main/java/br/ufrn/locationtracker/Api/OnJSONObjectResponseCallback.java
+++
@@ -1,7 +0,0 @@
-package br.ufrn.locationtracker.Api;
-
-import org.json.JSONObject;
-
-public interface OnJSONObjectResponseCallback {
-    void onJSONObjectResponse(boolean success, JSONObject response);
-}
